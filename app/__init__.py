aaf23a192c645cef2805e5eea14b264a523daa1e
Berry-More <108866004+Berry-More@users.noreply.github.com>
2023-02-26 17:56:30 +0700

    Delete app directory

--- app/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from config import Config
-
-app = Flask(__name__)
-app.config.from_object(Config)
-
-client = app.test_client()
-
-from app import routes
--- app/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
-
-
-class FriendsForm(FlaskForm):
-    first_name = StringField('1 user:', validators=[DataRequired()])
-    second_name = StringField('2 user:', validators=[DataRequired()])
-    submit = SubmitField('Enter')
